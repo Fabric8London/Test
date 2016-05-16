f2a9a3a1fde5bdaada0f8f12c0cbcdd39570d0b6
Nick <nick.lim@fabric-8.co.uk>
2016-05-16 10:18:13 +0100

    intial commit

---
+++ Business/PackagesBiz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public static class PackagesBiz
+    {
+        public static string GetAllPackagesJson()
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://test.client.fabric8.co.uk/api/packages/66");
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
